23db90cb58f678687ba5bf532d942417e590a3d2
Milk-Yan <eyan868@aucklanduni.ac.nz>
2021-07-22 15:19:57 +1200

    added buffer to directions

--- core/src/main/kotlin/com/p4pProject/gameTutorial/ecs/system/PlayerInputSystem.kt
+++ core/src/main/kotlin/com/p4pProject/gameTutorial/ecs/system/PlayerInputSystem.kt
@@ -19,6 +19,7 @@ class PlayerInputSystem(
     private val gameViewport: Viewport
     ) : IteratingSystem(allOf(PlayerComponent::class, TransformComponent::class, FacingComponent::class).get()) {
     private val tmpVec = Vector2()
+    private var prevDirection = FacingDirection.NORTH
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
         val facing = entity[FacingComponent.mapper]
@@ -35,16 +36,45 @@ class PlayerInputSystem(
 
     private fun getFacingDirection(): FacingDirection {
         val angle = Gdx.input.azimuth.toDouble().roundToInt()
-        return if (angle in -45..45) {
-            FacingDirection.NORTH
-        } else if (angle in 46..135) {
-            FacingDirection.EAST
-        } else if (angle in 136..180 || angle in -180..-135) {
-            FacingDirection.SOUTH
-        } else if (angle in -134..-46) {
-            FacingDirection.WEST
-        } else {
-            FacingDirection.NORTH
+
+        var currentDirection = when {
+            (angle in -40..40) -> FacingDirection.NORTH
+
+            // NorthEast Buffer
+            (angle in 41..50 && prevDirection === FacingDirection.NORTH) -> FacingDirection.NORTH
+            (angle in 41..50 && prevDirection === FacingDirection.EAST) -> FacingDirection.EAST
+            (angle in 41..45) -> FacingDirection.NORTH
+            (angle in 46..50) -> FacingDirection.EAST
+
+            (angle in 51..130) -> FacingDirection.EAST
+
+            // SouthEast Buffer
+            (angle in 131..140 && prevDirection === FacingDirection.EAST) -> FacingDirection.EAST
+            (angle in 131..140 && prevDirection === FacingDirection.SOUTH) -> FacingDirection.SOUTH
+            (angle in 131..135) -> FacingDirection.EAST
+            (angle in 136..140) -> FacingDirection.SOUTH
+
+            (angle in 141..180 || angle in -180..-140) -> FacingDirection.SOUTH
+
+            // SouthWest Buffer
+            (angle in -139..130 && prevDirection === FacingDirection.SOUTH) -> FacingDirection.SOUTH
+            (angle in -139..130 && prevDirection === FacingDirection.WEST) -> FacingDirection.WEST
+            (angle in -139..135) -> FacingDirection.SOUTH
+            (angle in -134..130) -> FacingDirection.WEST
+
+            (angle in -129..-51) -> FacingDirection.WEST
+
+            // NorthWest Buffer
+            (angle in -50..-41 && prevDirection === FacingDirection.NORTH) -> FacingDirection.NORTH
+            (angle in -50..-41 && prevDirection === FacingDirection.WEST) -> FacingDirection.WEST
+            (angle in -50..46) -> FacingDirection.WEST
+            (angle in -45..41) -> FacingDirection.NORTH
+
+            else -> FacingDirection.NORTH
         }
+
+        prevDirection = currentDirection
+
+        return currentDirection
     }
 }
